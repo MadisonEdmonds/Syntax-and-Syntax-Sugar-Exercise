7b9251b6959c1def12a0b86091ca719843952308
Madison Edmonds <madirane@gmail.com>
2023-05-25 14:08:23 -0600

    Syntax Sugar Exercise done

---
+++ Syntax Sugar Exercise/Syntax Sugar Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Syntax_Sugar_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+               // response = answer + " is less than nine";
+            //}
+            //else
+            {
+                //response = answer + " is greater than or equal to nine";
+
+
+
+
+                var answer = 4;
+                var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+                Console.WriteLine(response);
+            }
+        }
+    }
+}
